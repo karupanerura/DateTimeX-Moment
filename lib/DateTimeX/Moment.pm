e28422dc2a4c74a1edc9fcfe47d715a7e5f146f4
chansen <chansen@cpan.org>
2016-04-21 00:19:22 +0200

    Group methods in a somewhat logical order

--- lib/DateTimeX/Moment.pm
+++ lib/DateTimeX/Moment.pm
@@ -288,24 +288,6 @@ sub day_of_year_0 { shift->{_moment}->day_of_year - 1 }
 sub quarter { shift->{_moment}->quarter }
 sub quarter_0 { shift->{_moment}->quarter - 1 }
 sub weekday_of_month { int((shift->{_moment}->day_of_month + 6) / 7) }
-sub hour { shift->{_moment}->hour }
-sub hour_1 { shift->{_moment}->hour || 24 }
-sub hour_12 { shift->hour_12_0 || 12 }
-sub hour_12_0 { shift->{_moment}->hour % 12 }
-sub minute { shift->{_moment}->minute }
-sub second { shift->{_moment}->second }
-
-sub fractional_second {
-    my $moment = shift->{_moment};
-    return $moment->second + $moment->nanosecond / 1_000_000_000;
-}
-
-sub nanosecond { shift->{_moment}->nanosecond }
-sub millisecond { shift->{_moment}->millisecond }
-sub microsecond { shift->{_moment}->microsecond }
-
-sub is_leap_year { shift->{_moment}->is_leap_year + 0 }
-sub leap_seconds { 0 } ## XXX: time moment doesn't have a leap seconds. So always leap seconds are zero.
 
 sub week_number { shift->{_moment}->week }
 sub week_year { shift->{_moment}->strftime('%G') + 0 }
@@ -323,7 +305,77 @@ sub week_of_month {
     return int(($thu + 6) / 7);
 }
 
+sub is_leap_year { shift->{_moment}->is_leap_year + 0 }
+
+sub hour { shift->{_moment}->hour }
+sub hour_1 { shift->{_moment}->hour || 24 }
+sub hour_12 { shift->hour_12_0 || 12 }
+sub hour_12_0 { shift->{_moment}->hour % 12 }
+sub minute { shift->{_moment}->minute }
+sub second { shift->{_moment}->second }
+sub nanosecond { shift->{_moment}->nanosecond }
+sub millisecond { shift->{_moment}->millisecond }
+sub microsecond { shift->{_moment}->microsecond }
+
+sub fractional_second {
+    my $moment = shift->{_moment};
+    return $moment->second + $moment->nanosecond / 1_000_000_000;
+}
+
+sub leap_seconds { 0 } ## XXX: time moment doesn't have a leap seconds. So always leap seconds are zero.
+
+sub mjd { $_[0]->{_moment}->mjd }
+sub jd { $_[0]->{_moment}->jd }
+sub rd { $_[0]->{_moment}->rd }
+
+sub epoch { shift->{_moment}->epoch }
+
+sub hires_epoch {
+    my $moment = shift->{_moment};
+    return $moment->epoch + $moment->nanosecond / 1_000_000_000;
+}
+
+# NOTE: no nanoseconds, no leap seconds
+sub utc_rd_values   { $_[0]->{_moment}->utc_rd_values }
+sub local_rd_values { $_[0]->{_moment}->local_rd_values }
+sub utc_rd_as_seconds   { $_[0]->{_moment}->utc_rd_as_seconds }
+sub local_rd_as_seconds { $_[0]->{_moment}->local_rd_as_seconds }
+
+sub is_finite   { 1 }
+sub is_infinite { 0 }
+
 sub offset { shift->{_moment}->offset * 60 }
+
+sub is_dst { $_[0]->{time_zone}->is_dst_for_datetime($_[0]) }
+
+sub time_zone_long_name  { $_[0]->{time_zone}->name }
+sub time_zone_short_name { $_[0]->{time_zone}->short_name_for_datetime($_[0]) }
+
+
+sub utc_year { shift->{_moment}->utc_year }
+
+## NOTE: Time::Moment supports date in anno Domini omly.
+sub ce_year { shift->{_moment}->year }
+sub era_name { $_[0]->{locale}->era_wide->[1] }
+sub era_abbr { $_[0]->{locale}->era_abbreviated->[1] }
+sub christian_era { 'AD' }
+sub secular_era   { 'CE' }
+sub year_with_era           { ( abs $_[0]->ce_year ) . $_[0]->era_abbr }
+sub year_with_christian_era { ( abs $_[0]->ce_year ) . $_[0]->christian_era }
+sub year_with_secular_era   { ( abs $_[0]->ce_year ) . $_[0]->secular_era }
+
+sub month_name { $_[0]->{locale}->month_format_wide->[ $_[0]->month_0() ] }
+sub month_abbr { $_[0]->{locale}->month_format_abbreviated->[ $_[0]->month_0() ] }
+sub day_name   { $_[0]->{locale}->day_format_wide->[ $_[0]->day_of_week_0() ] }
+sub day_abbr   { $_[0]->{locale}->day_format_abbreviated->[ $_[0]->day_of_week_0() ] }
+sub am_or_pm   { $_[0]->{locale}->am_pm_abbreviated->[ $_[0]->{_moment}->hour < 12 ? 0 : 1 ] }
+sub quarter_name { $_[0]->{locale}->quarter_format_wide->[ $_[0]->quarter_0() ] }
+sub quarter_abbr { $_[0]->{locale}->quarter_format_abbreviated->[ $_[0]->quarter_0() ] }
+
+sub local_day_of_week {
+    my $self = shift;
+    return 1 + ($self->{_moment}->day_of_week - $self->{locale}->first_day_of_week) % 7;
+}
 
 sub _escape_pct {
     (my $string = $_[0]) =~ s/%/%%/g; $string;
@@ -360,50 +412,6 @@ sub hms {
 sub iso8601 {
     return $_[0]->{_moment}->strftime('%Y-%m-%dT%H:%M:%S');
 }
-
-# NOTE: no nanoseconds, no leap seconds
-sub utc_rd_values   { $_[0]->{_moment}->utc_rd_values }
-sub local_rd_values { $_[0]->{_moment}->local_rd_values }
-sub utc_rd_as_seconds   { $_[0]->{_moment}->utc_rd_as_seconds }
-sub local_rd_as_seconds { $_[0]->{_moment}->local_rd_as_seconds }
-sub utc_year { shift->{_moment}->utc_year }
-
-## NOTE: Time::Moment supports date in anno Domini omly.
-sub ce_year { shift->{_moment}->year }
-sub era_name { $_[0]->{locale}->era_wide->[1] }
-sub era_abbr { $_[0]->{locale}->era_abbreviated->[1] }
-sub christian_era { 'AD' }
-sub secular_era   { 'CE' }
-sub year_with_era           { ( abs $_[0]->ce_year ) . $_[0]->era_abbr }
-sub year_with_christian_era { ( abs $_[0]->ce_year ) . $_[0]->christian_era }
-sub year_with_secular_era   { ( abs $_[0]->ce_year ) . $_[0]->secular_era }
-
-sub month_name { $_[0]->{locale}->month_format_wide->[ $_[0]->month_0() ] }
-sub month_abbr { $_[0]->{locale}->month_format_abbreviated->[ $_[0]->month_0() ] }
-sub day_name   { $_[0]->{locale}->day_format_wide->[ $_[0]->day_of_week_0() ] }
-sub day_abbr   { $_[0]->{locale}->day_format_abbreviated->[ $_[0]->day_of_week_0() ] }
-sub am_or_pm   { $_[0]->{locale}->am_pm_abbreviated->[ $_[0]->{_moment}->hour < 12 ? 0 : 1 ] }
-sub quarter_name { $_[0]->{locale}->quarter_format_wide->[ $_[0]->quarter_0() ] }
-sub quarter_abbr { $_[0]->{locale}->quarter_format_abbreviated->[ $_[0]->quarter_0() ] }
-
-sub local_day_of_week {
-    my $self = shift;
-    return 1 + ($self->{_moment}->day_of_week - $self->{locale}->first_day_of_week) % 7;
-}
-
-sub mjd { $_[0]->{_moment}->mjd }
-sub jd { $_[0]->{_moment}->jd }
-sub rd { $_[0]->{_moment}->rd }
-
-sub epoch { shift->{_moment}->epoch }
-
-sub hires_epoch {
-    my $moment = shift->{_moment};
-    return $moment->epoch + $moment->nanosecond / 1_000_000_000;
-}
-
-sub is_finite   { 1 }
-sub is_infinite { 0 }
 
 sub subtract_datetime {
     my ($lhs, $rhs) = @_;
@@ -630,6 +638,39 @@ sub set_minute     { $_[0]->set(minute     => $_[1]) }
 sub set_second     { $_[0]->set(second     => $_[1]) }
 sub set_nanosecond { $_[0]->set(nanosecond => $_[1]) }
 
+sub set_time_zone {
+    my ($self, $time_zone) = @_;
+    Carp::croak 'required time_zone' if @_ != 2;
+
+    $time_zone = $self->_inflate_time_zone($time_zone);
+    return $self if $time_zone == $self->{time_zone};
+    return $self if $time_zone->name eq $self->{time_zone}->name;
+
+    $self->{_moment} = do {
+        if ($self->{time_zone}->is_floating) {
+            _moment_resolve_local($self->{_moment}, $time_zone)
+        }
+        else {
+            _moment_resolve_instant($self->{_moment}, $time_zone);
+        }
+    };
+    $self->{time_zone} = $time_zone;
+    return $self;
+}
+
+sub set_locale {
+    my ($self, $locale) = @_;
+    Carp::croak 'required locale' if @_ != 2;
+    $self->{locale} = $self->_inflate_locale($locale);
+    return $self;
+}
+
+sub set_formatter {
+    my ($self, $formatter) = @_;
+    $self->{formatter} = $self->_inflate_formatter($formatter);
+    return $self;
+}
+
 sub add      { shift->_calc_date(plus  => @_) }
 sub subtract { shift->_calc_date(minus => @_) }
 
@@ -814,31 +855,6 @@ sub format_cldr {
     )->format_cldr(@_);
 }
 
-sub set_time_zone {
-    my ($self, $time_zone) = @_;
-    Carp::croak 'required time_zone' if @_ != 2;
-
-    $time_zone = $self->_inflate_time_zone($time_zone);
-    return $self if $time_zone == $self->{time_zone};
-    return $self if $time_zone->name eq $self->{time_zone}->name;
-
-    $self->{_moment} = do {
-        if ($self->{time_zone}->is_floating) {
-            _moment_resolve_local($self->{_moment}, $time_zone)
-        }
-        else {
-            _moment_resolve_instant($self->{_moment}, $time_zone);
-        }
-    };
-    $self->{time_zone} = $time_zone;
-    return $self;
-}
-
-sub is_dst { $_[0]->{time_zone}->is_dst_for_datetime($_[0]) }
-
-sub time_zone_long_name  { $_[0]->{time_zone}->name }
-sub time_zone_short_name { $_[0]->{time_zone}->short_name_for_datetime($_[0]) }
-
 sub truncate :method {
     my $self = shift;
     my %args = (@_ == 1 && ref $_[0] eq 'HASH') ? %{$_[0]} : @_;
@@ -912,19 +928,6 @@ sub add_duration {
     }
 
     return $self->add($duration->deltas);
-}
-
-sub set_locale {
-    my ($self, $locale) = @_;
-    Carp::croak 'required locale' if @_ != 2;
-    $self->{locale} = $self->_inflate_locale($locale);
-    return $self;
-}
-
-sub set_formatter {
-    my ($self, $formatter) = @_;
-    $self->{formatter} = $self->_inflate_formatter($formatter);
-    return $self;
 }
 
 # internal utilities
